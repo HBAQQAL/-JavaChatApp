1eece91b62baf3c219da0f933372c59c3a6bcf9e
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-12-02 16:11:58 +0100

    New : defining paths

---
+++ javaserver/src/main/java/com/javaserver/utils/Path.java
@@ -0,0 +1,12 @@
+package com.javaserver.utils;
+
+public class Path {
+    public static class API {
+
+        public static final String USERS = "/api/users";
+        public static final String LOGIN = "/api/login";
+        public static final String LOGOUT = "/api/logout";
+
+
+    }
+}
