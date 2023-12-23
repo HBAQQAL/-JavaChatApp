69d888dfa33c9f1b1ccf5fbdc8abd97860ccbe54
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-12-23 12:43:29 +0100

    Add CoockieHandler utility class

---
+++ client/src/main/java/com/client/utils/CoockieHandler.java
@@ -0,0 +1,41 @@
+package com.client.utils;
+
+public class CoockieHandler {
+    private static CoockieHandler instance;
+    private String coockie;
+
+    private CoockieHandler() {
+        // Private constructor to prevent instantiation from outside the class
+    }
+
+    public static CoockieHandler getInstance() {
+        if (instance == null) {
+            synchronized (CoockieHandler.class) {
+                if (instance == null) {
+                    instance = new CoockieHandler();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String getCoockie() {
+        return coockie;
+    }
+
+    public void setCoockie(String coockie) {
+        this.coockie = coockie;
+    }
+
+    public static String getCoockie(String coockie) {
+        return coockie.split(";")[0];
+    }
+
+    public static String getCoockieFromHeader(String header) {
+        return header.split(";")[0];
+    }
+
+    public void setCoockieFromHeader(String header) {
+        this.coockie = header.split(";")[0];
+    }
+}
