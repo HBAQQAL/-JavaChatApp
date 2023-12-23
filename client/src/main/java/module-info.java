3d72bea37fdc6544425dfa99b28a99751b9dde5a
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-12-23 12:45:07 +0100

    Add transitive dependency and Gson requirement, open controllers package to JavaFX.fxml

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
+    requires com.google.gson;
 
     opens com.client to javafx.fxml;
+    opens com.client.controllers to javafx.fxml;
 
     exports com.client;
 }
