d164d6797a61682224036f2c48b80cadd0c320f9
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-12-23 12:44:45 +0100

    Add GsonJsonMapper for JSON serialization and deserialization

--- javaserver/src/main/java/com/javaserver/Main.java
+++ javaserver/src/main/java/com/javaserver/Main.java
@@ -1,10 +1,12 @@
 package com.javaserver;
 
+import java.lang.reflect.Type;
 
+import com.google.gson.Gson;
 import com.javaserver.routes.AuthRoutes;
 import com.javaserver.routes.UserRoutes;
-
 import com.javaserver.utils.Sessions;
+
 import io.javalin.Javalin;
 import io.javalin.json.JavalinJackson;
 
@@ -13,6 +15,7 @@ public class Main {
         Javalin app = Javalin.create(config -> {
             config.jsonMapper(new JavalinJackson());
             config.jetty.sessionHandler(() -> Sessions.fileSessionHandler());
+            config.jsonMapper(new GsonJsonMapper());
         });
         app.error(404, ctx -> {
             ctx.result("Not found");
@@ -22,5 +25,34 @@ public class Main {
 
         app.start(7000);
     }
-}
 
+    static class GsonJsonMapper implements io.javalin.json.JsonMapper {
+
+        private final Gson gson = new Gson();
+
+        @Override
+        public <T> T fromJsonString(String json, Type targetType) {
+            return gson.fromJson(json, targetType);
+        }
+
+        @Override
+        public String toJsonString(Object obj, Type type) {
+            return gson.toJson(obj, type);
+        }
+
+        // @Override
+        // public String toJson(Object obj) {
+        // return gson.toJson(obj);
+        // }
+
+        // @Override
+        // public <T> T fromJson(String json, Class<T> targetClass) {
+        // return gson.fromJson(json, targetClass);
+        // }
+
+        // @Override
+        // public <T> T fromJson(String json, TypeToken<T> targetType) {
+        // return gson.fromJson(json, targetType.getType());
+        // }
+    }
+}
