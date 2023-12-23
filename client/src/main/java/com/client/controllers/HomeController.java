0f63f171269fae5b4ac55b88766a520b346d7367
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-12-23 12:41:46 +0100

    Add HomeController and home.fxml files

---
+++ client/src/main/java/com/client/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.client.controllers;
+
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    public void handleButtonClick() {
+        // Add your login logic here (e.g., check credentials, open a new scene, etc.)
+        System.out.println("Logging in with username: ");
+    }
+}
